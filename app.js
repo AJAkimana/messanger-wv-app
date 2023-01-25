9c3fcea3add7946829de34bba06344534fa5fd8a
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-01-25 11:18:43 +0200

    Check body

--- app.js
+++ app.js
@@ -75,13 +75,13 @@ app.get("/optionspostback", (req, res) => {
 app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
-
+  console.log("===>Body", body);
   // Check the webhook event is from a Page subscription
   if (body.object === "page") {
     body.entry.forEach((entry) => {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      // console.log(webhook_event);
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
